1ed9a4f6474fc9d859ca7e95fe80090c74084691
fiebryhoga <dimasfiebry@gmail.com.com>
2024-05-28 00:44:50 +0700

    Resizing ToolsCard in Mobile

--- app/components/tools/ToolsCard.jsx
+++ app/components/tools/ToolsCard.jsx
@@ -6,8 +6,8 @@ const ToolsCard = ({ tool }) => {
   return (
     <Link
       href={tool.link}
-      className="py-4 pl-[40px] h-20 xl:w-full flex justify-start items-center border rounded-lg border-gray-200 border-opacity-60 flex-row gap-4
-      hover:bg-white hover:transition-all hover:duration-300 transition-all duration-500 text-base text-white text-opacity-80 font-medium hover:text-black tracking-wide"
+      className="py-4 md:pl-[40px] h-24 md:h-20 xl:w-full flex justify-center md:justify-start items-center border rounded-lg border-gray-200 border-opacity-60 md:flex-row flex-col gap-1 md:gap-4
+      hover:bg-white hover:transition-all hover:duration-300 transition-all duration-500 text-sm md:text-base text-white text-opacity-80 font-medium hover:text-black tracking-wide"
     >
       <img className="w-9" src={tool.image} alt={tool.nama} />
       <p className="">{tool.nama}</p>
